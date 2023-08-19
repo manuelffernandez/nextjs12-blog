1f8369c6baad461e2a2f6bb4b65b309eb52f982f
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-08-19 17:26:48 -0300

    update profile image

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Your Name';
+const name = 'Manuel Fernandez';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
@@ -32,8 +32,9 @@ export default function Layout({ children, home }) {
               priority
               src="/images/profile.jpg"
               className={utilStyles.borderCircle}
-              height={144}
+              style={{ objectFit: 'cover' }}
               width={144}
+              height={144}
               alt=""
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
@@ -45,8 +46,9 @@ export default function Layout({ children, home }) {
                 priority
                 src="/images/profile.jpg"
                 className={utilStyles.borderCircle}
-                height={108}
-                width={108}
+                style={{ objectFit: 'cover' }}
+                width={144}
+                height={144}
                 alt=""
               />
             </Link>
